e7c688b4bfd5b6d4c7e6e41aba1fd53b4d35f59a
Fizan Muhammed Faisal <fizanmuhammedfaisal.k@gmail.com>
2025-08-21 10:14:40 +0530

    turned off tsc and other ts for deploying. will fix errros later

--- src/pages/user/Testing.tsx
+++ src/pages/user/Testing.tsx
@@ -1,71 +1,7 @@
-import { useState } from 'react'
-import {
-   DndContext,
-   closestCenter,
-   KeyboardSensor,
-   PointerSensor,
-   useSensor,
-   useSensors,
-   type DragEndEvent,
-} from '@dnd-kit/core'
-import {
-   arrayMove,
-   SortableContext,
-   sortableKeyboardCoordinates,
-   useSortable,
-   verticalListSortingStrategy,
-} from '@dnd-kit/sortable'
-import { CSS } from '@dnd-kit/utilities'
-function App() {
-   const [items, setItems] = useState([1, 2, 3])
+import React from 'react'
 
-   const sensors = useSensors(
-      useSensor(PointerSensor),
-      useSensor(KeyboardSensor, {
-         coordinateGetter: sortableKeyboardCoordinates,
-      }),
-   )
-
-   return (
-      <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-         <SortableContext items={items} strategy={verticalListSortingStrategy}>
-            {items.map((id) => (
-               <SortableItem key={id} id={id} />
-            ))}
-         </SortableContext>
-      </DndContext>
-   )
-
-   function handleDragEnd(event: DragEndEvent) {
-      const { active, over } = event
-
-      if (active.id !== over.id) {
-         setItems((items) => {
-            const oldIndex = items.indexOf(active.id)
-            const newIndex = items.indexOf(over.id)
-
-            return arrayMove(items, oldIndex, newIndex)
-         })
-      }
-   }
+function Testing() {
+   return <div>Testing</div>
 }
-export default App
 
-export function SortableItem(props) {
-   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
-      id: props.id,
-   })
-
-   const style = {
-      transform: CSS.Transform.toString(transform),
-      transition,
-   }
-
-   return (
-      <div ref={setNodeRef} style={style} className="text-white" {...attributes} {...listeners}>
-         <div className="p-4 mt-2 mx-2 bg-amber-800">
-            <span>{props.id}</span>
-         </div>
-      </div>
-   )
-}
+export default Testing
